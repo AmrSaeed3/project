849b92b0e153f96866c5b6eb057759e7c4b882ed
AmrSaeed3 <amrsaid4343@gmail.com>
2024-04-23 19:45:42 +0200

    cont

--- Controllers/user.controllers.js
+++ Controllers/user.controllers.js
@@ -35,7 +35,7 @@ const register = asyncWrapper(async (req, res, next) => {
   });
   await newUser.save();
   return res.status(200).json({
-    status: "SUCCESS",
+    status: "success",
   });
   // const redirectUrl = `/verify?userName=${userName}&email=${email}&password=${password}&token=${token}`;
   // res.redirect(redirectUrl)
