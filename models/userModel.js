5b3e546000ec6b0c3a329859a8a20c9838c9527d
MazenMostafaSW <mazen.mostafa.s.w.eng@gmail.com>
2025-06-15 17:55:36 +0300

    change birthdate to birthDate in user model

--- models/userModel.js
+++ models/userModel.js
@@ -73,7 +73,7 @@ const userSchema = new mongoose.Schema(
         country: {
             type: String
         },
-        birthday: {
+        birthDate: {
             type: Date
         },
         gender: {
