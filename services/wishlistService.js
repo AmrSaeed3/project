05675085b6ff17a9c64fda11b44278e70a12483e
MazenMostafaSW <mazen.mostafa.s.w.eng@gmail.com>
2025-06-28 16:10:39 +0300

    improve the wishlist

--- services/wishlistService.js
+++ services/wishlistService.js
@@ -6,14 +6,13 @@ const ApiError = require('../utils/apiError');
 // Add product to wishlist
 exports.addToWishlist = asyncHandler(async (req, res, next) => {
     const user = await User.findByIdAndUpdate(
-        req.user.id, 
-        {
-        $addToSet: { wishlist: req.params.productId }
-        }, 
-        {
-            new: true
-        });
-
+        req.user.id,
+        { $addToSet: { wishlist: req.params.productId } },
+        { new: true }
+    );
+    if (!user) {
+        return next(new ApiError('User not found', 404));
+    }
     res.status(200).json({
         status: 'success',
         message: 'Product added to wishlist',
@@ -24,14 +23,13 @@ exports.addToWishlist = asyncHandler(async (req, res, next) => {
 // Remove product from wishlist
 exports.removeFromWishlist = asyncHandler(async (req, res, next) => {
     const user = await User.findByIdAndUpdate(
-        req.user.id, 
-        {
-        $pull: { wishlist: req.params.productId }
-        }
-    , {
-        new: true
-    });
-
+        req.user.id,
+        { $pull: { wishlist: req.params.productId } },
+        { new: true }
+    );
+    if (!user) {
+        return next(new ApiError('User not found', 404));
+    }
     res.status(200).json({
         status: 'success',
         message: 'Product removed from wishlist',
@@ -41,15 +39,19 @@ exports.removeFromWishlist = asyncHandler(async (req, res, next) => {
 
 // Get user wishlist
 exports.getWishlist = asyncHandler(async (req, res, next) => {
+    // Populate wishlist with product details
     const user = await User.findById(req.user.id).populate({
         path: 'wishlist',
-        select: 'name imageCover price slug ratingsAverage ratingsQuantity description'
+        select: 'name imageCover price slug quantity ratingsAverage ratingsQuantity description'
     });
+
+    if (!user || !user.wishlist || user.wishlist.length === 0) {
+        return next(new ApiError('No wishlist found for this user', 404));
+    }
 
     res.status(200).json({
         status: 'success',
         results: user.wishlist.length,
         data: user.wishlist
     });
-    next();
 });
